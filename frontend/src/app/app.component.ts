0429525f049cde0c79842fda48776a104ec0c0b1
nawaf <nawafhunter@gmail.com>
2024-11-04 12:37:55 +0300

    home page components

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DetailesComponent } from './detailes/detailes.component';
+import { FeaturesComponent } from './features/features.component';
 import { HeaderComponent } from './header/header.component';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, DetailesComponent, FeaturesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
